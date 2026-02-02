e0d532375d9ab8559e2fb4879909f38de4fb83aa
AB <ab@6g.ru>
2026-02-02 11:27:54 +0300

    4sprintv1

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,13 @@
 package daysteps
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -11,10 +17,52 @@ const (
 	mInKm = 1000
 )
 
+// parsePackage парсит строку формата "678,0h50m", где:
+// 678 — количество шагов.
+// 0h50m — продолжительность прогулки.
+// Переводит эти данные в int и time.Duration соответственно и возвращает их.
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, fmt.Errorf("incorrect data \"%s\" in parsePackage()", data)
+	}
+	num, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return 0, 0, fmt.Errorf("steps parsing error: %s in parsePackage()", err)
+	}
+	if num <= 0 {
+		return 0, 0, fmt.Errorf("steps number equal or below zero. data: %s in parsePackage()", data)
+	}
+	dur, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return 0, 0, fmt.Errorf("duraion parsing error: %s in parsePackage()", err)
+	}
+	if dur <= 0 {
+		return 0, 0, fmt.Errorf("duration equal or below zero. data: %s in parsePackage()", data)
+	}
+	return num, dur, nil
 }
 
+// DayActionInfo парсит строку с данными с помощью parsePackage(),
+// вычисляет дистанцию в километрах, количество потраченных калорий и возвращает строку в виде:
+// Количество шагов: 792.
+// Дистанция составила 0.51 км.
+// Вы сожгли 221.33 ккал.
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	numOfSteps, walkDuration, err := parsePackage(data)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	if numOfSteps < 0 {
+		return ""
+	}
+	distanceInKm := float64(numOfSteps) * stepLength / mInKm
+	calories, err := spentcalories.WalkingSpentCalories(numOfSteps, weight, height, walkDuration)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return fmt.Sprintf("Количество шагов: %v.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		numOfSteps, distanceInKm, calories)
 }
